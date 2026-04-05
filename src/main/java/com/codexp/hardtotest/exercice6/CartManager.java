14d1f32f81087a862f9609399805c3b68a57f892
Drou <consulting@pascaldrouin.com>
2026-04-04 20:04:16 -0400

    revised the comments... they were too obvious

--- src/main/java/com/codexp/hardtotest/exercice6/CartManager.java
+++ src/main/java/com/codexp/hardtotest/exercice6/CartManager.java
@@ -1,12 +1,10 @@
 package com.codexp.hardtotest.exercice6;
 
 public class CartManager {
-    //we are searching through the object tree the things we need instead of asking for it.
-    //Do we need to know about Customer... at all?
-    //What about Order?  It is very likely to change sooner or later to remove its unhealthy relationship with PaymentService, which will affect this class...
-    //Other than binding ourselves to unwanted dependencies, how this situation augment complexity doing unit testing? (Arrange phase for instance)
-    //Here, most of our calls are accessor... what if we had more logic call chained like processTransaction?
-    //Do you see any other problems with chaining like that?
+    //Do we need to know about all these dependencies that we are accidentally inviting to the party?
+    //hwo does chaining affect the AAA of unit testing?
+    //Accessor chains vs Execution chains
+    //Ask for things, don't look for them
     public String process(Customer customer) {
         return customer.getOrder().getPayment().processTransaction(customer.getOrder().getItem().getPrice());
     }
