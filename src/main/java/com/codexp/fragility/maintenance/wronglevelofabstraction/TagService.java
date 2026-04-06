838949cd5fdd5d9f9bfaa34be08627d8b72ec4e8
Drou <consulting@pascaldrouin.com>
2026-04-06 11:01:30 -0400

    remove a commetn

--- src/main/java/com/codexp/fragility/maintenance/wronglevelofabstraction/TagService.java
+++ src/main/java/com/codexp/fragility/maintenance/wronglevelofabstraction/TagService.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class TagService {
     public Collection<String> getTags() {
-        // Implementation detail: Using TreeSet to keep things alphabetical
         Set<String> tags = new TreeSet<>();
 
         //Set<String> tags = new HashSet<>();
